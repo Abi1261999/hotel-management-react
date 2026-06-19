73937a0211a256fde2462529f747174ae4cb81e5
Abi1261999 <abisheksingh.as55@gmail.com>
2026-06-19 21:57:35 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import HotelProvider from "./context/HotelContext";
 import "./index.css";
 
 ReactDOM.createRoot(
